ad6174df9f32d511acc52f2e2e3db547a9ad99d8
Mateus <120814794+MateusCgoldman@users.noreply.github.com>
2023-01-07 21:58:09 -0300

    teste.py

---
+++ teste.py
@@ -0,0 +1,39 @@
+print("Hello World!")
+
+def new_func():
+    new_func1()
+
+def new_func1():
+    new_func2()
+
+def new_func2():
+    new_func3()
+
+def new_func3():
+    new_var = new_func4()
+    new_var
+
+def new_func4():
+    return new_func5()
+
+def new_func5():
+    return new_func6()
+
+def new_func6():
+    new_var = new_func7()
+    return new_var
+
+def new_func7():
+    return new_func8()
+
+def new_func8():
+    return new_func9()
+
+def new_func9():
+    new_var = new_func10()
+    return new_var
+
+def new_func10():
+    return
+
+new_func()> type(1)
